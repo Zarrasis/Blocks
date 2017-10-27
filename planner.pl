c570de98704f3cce7688501f9b626666c0ee1ca7
Matt Taubler <Zarrasis@Matts-Air.attlocal.net>
2017-10-27 03:21:35 -0400

    Not final, test cases 1, 2, not working

--- planner.pl
+++ planner.pl
@@ -9,17 +9,25 @@
 %%% by George F. Luger and William A. Stubblefield
 %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%% Two Room Planner by Matthew Taubler and Brooke Norton
+
+
+%add test(x)/0
 :- module( planner,
 	   [
 	       plan/4,change_state/3,conditions_met/2,member_state/2,
-	       move/3,go/2,test/0,test2/0
+	       move/3,go/2,test/0,test2/0,test4/0
 	   ]).
 
 :- [utils].
 
+% if the sets state and goal are equal, we can print the moves
 plan(State, Goal, _, Moves) :-	equal_set(State, Goal),
 				write('moves are'), nl,
 				reverse_print_stack(Moves).
+
+%no idea, i think this is where we might IDFS
 plan(State, Goal, Been_list, Moves) :-
 				move(Name, Preconditions, Actions),
 				conditions_met(Preconditions, State),
@@ -41,38 +49,51 @@ member_state(S, [_|T]) :-	member_state(S, T).
 
 /* move types */
 
-move(pickup(X), [handempty, clear(X), on(X, Y)],
-		[del(handempty), del(clear(X)), del(on(X, Y)),
+% pickup off another block
+% see if X is on block Y in room R, also if hand is in room R
+move(pickup(X), [handempty, clear(X), on(X, Y, R), handroom(R)],
+		[del(handempty), del(clear(X)), del(on(X, Y, R)),
 				 add(clear(Y)),	add(holding(X))]).
 
-move(pickup(X), [handempty, clear(X), ontable(X)],
-		[del(handempty), del(clear(X)), del(ontable(X)),
+% pick up from table
+% see if x is on the table in room R, and hand is in room R
+move(pickup(X), [handempty, clear(X), ontable(X,R), handroom(R)],
+		[del(handempty), del(clear(X)), del(ontable(X,R)),
 				 add(holding(X))]).
 
-move(putdown(X), [holding(X)],
-		[del(holding(X)), add(ontable(X)), add(clear(X)),
+% put down on table
+% if you are in the correct room R add X to the table in that room R
+move(putdown(X), [holding(X), handroom(R)],
+		[del(holding(X)), add(ontable(X,R)), add(clear(X)),
 				  add(handempty)]).
 
-move(stack(X, Y), [holding(X), clear(Y)],
-		[del(holding(X)), del(clear(Y)), add(handempty), add(on(X, Y)),
+% put down on stack
+% if you are holding x and block y is clear and you are in room r, then
+% add on to that block, remove y from clear and stack x on y in r
+move(stack(X, Y), [holding(X), clear(Y), handroom(R)],
+		[del(holding(X)), del(clear(Y)), add(handempty), add(on(X,Y,R)),
 				  add(clear(X))]).
 
 % go to room 1
-move(goroom(1),[handroom(2)],
-	[del(handroom(2)),
+move(goroom(1),
+	[handroom(2)], [del(handroom(2)),
 		add(handroom(1))]).
 
 % go to room 2
-move(goroom(2),[handroom(1)],
-	[del(handroom(1)),
+move(goroom(2),
+	[handroom(1)],	[del(handroom(1)),
 		add(handroom(2))]).
 
 /* run commands */
 
 go(S, G) :- plan(S, G, [S], []).
 
-test :- go([handempty, ontable(b), ontable(c), on(a, b), clear(c), clear(a)],
-	          [handempty, ontable(c), on(a,b), on(b, c), clear(a)]).
+test :- go([handempty, ontable(b,1), ontable(c,1), on(a, b,1), clear(c), clear(a), handroom(1)],
+	          [handempty, ontable(c,1), on(a,b,1), on(b, c,1), clear(a), handroom(1)]).
 
 test2 :- go([handempty, ontable(b), ontable(c), on(a, b), clear(c), clear(a)],
 	          [handempty, ontable(a), ontable(b), on(c, b), clear(a), clear(c)]).
+
+% 1 or 2 for what room its in
+test4 :- go([handempty, ontable(b,1),on(a, b, 1), clear(a), handroom(1)],
+							[handempty, ontable(b, 2), on(a, b, 2),clear(a),  handroom(1)]).
